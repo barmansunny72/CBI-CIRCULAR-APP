a5b6867f4c6cd75dc185fdb6db78f23c571b5ba2
barmansunny72 <barmansunny72@gmail.com>
2026-06-06 01:25:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def check_password():
 
 # --- AI & DRIVE SETUP ---
 genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
-model = genai.GenerativeModel('gemini-1.5-flash')
+model = genai.GenerativeModel('gemini-1.5-pro-latest')
 
 @st.cache_resource
 def get_drive_service():
